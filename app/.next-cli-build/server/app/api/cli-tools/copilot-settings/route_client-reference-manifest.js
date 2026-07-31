c7956471f061e89c4701bd0b2fe3decceab63da3
mohamed7546 <mohamed701164@gmail.com>
2026-07-31 19:34:57 +0300

    Upload official 9router original file: app/.next-cli-build/server/app/api/cli-tools/copilot-settings/route_client-reference-manifest.js

---
+++ app/.next-cli-build/server/app/api/cli-tools/copilot-settings/route_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/api/cli-tools/copilot-settings/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/Users/Working/router4/9router/src/app/api/cli-tools/copilot-settings/route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}};
