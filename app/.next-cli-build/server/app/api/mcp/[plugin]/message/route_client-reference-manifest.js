bf1b8557753cd51c5f5eccd41828b68ffb7a8859
mohamed7546 <mohamed701164@gmail.com>
2026-07-31 19:36:36 +0300

    Upload official 9router original file: app/.next-cli-build/server/app/api/mcp/[plugin]/message/route_client-reference-manifest.js

---
+++ app/.next-cli-build/server/app/api/mcp/[plugin]/message/route_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/api/mcp/[plugin]/message/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/Users/Working/router4/9router/src/app/api/mcp/[plugin]/message/route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}};
