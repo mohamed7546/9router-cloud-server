c415a4acd3f99523e67a67652cbab9f6bdaa8fc8
mohamed7546 <mohamed701164@gmail.com>
2026-07-31 23:15:44 +0300

    Add Next.js root entrypoint files

---
+++ app/pages/index.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem", textAlign: "center" }}>
+      <h1>🚀 9Router Cloud Server</h1>
+      <p>Your official 9router instance is running online 24/7 cloud-native.</p>
+    </div>
+  );
+}
